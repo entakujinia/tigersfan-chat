9f6636a7b424a6c80ad9107f6fd9faf4cb61ee02
entakujinia <takusnn@icloud.com>
2020-10-29 18:34:05 +0900

    deviseに関するビューの生成とvalidationの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :nickname,        presence: true
+  validates :fan_history,     presence: true
+  validates :favorite_player, presence: true
 end
