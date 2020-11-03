d9b5a869dd44de6f97282d425ea902bc4c8f1f85
entakujinia <takusnn@icloud.com>
2020-11-03 16:15:10 +0900

    ヘッダーリストの色変更

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,11 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
     registrations: 'users/registrations'
   }
-
+  
   devise_scope :user do
     post '/users/guest_sign_in', to: 'users/sessions#new_guest'
   end
 
   root to: "places#index"
+  resources :places
 end 
