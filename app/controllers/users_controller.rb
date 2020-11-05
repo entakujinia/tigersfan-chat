f0036614e6fa467bc7ffc0fd8c7b3e79b36f153e
entakujinia <takusnn@icloud.com>
2020-11-05 11:17:33 +0900

    コントローラーにアクション追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,19 @@ class UsersController < ApplicationController
   def show
     @user = User.all
   end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    user = User.find(params[:id])
+    user.update(user_params)
+  end
+
+private
+
+  def user_params
+    params.require(:user).permit(:nickname, :email, :password, :password_confirmation, :fan_history_id, :favorite_player_id)
+  end
 end
