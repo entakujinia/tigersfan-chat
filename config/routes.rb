3502bf0a65c083ec5ec305fa5052515d0641c124
entakujinia <takusnn@icloud.com>
2020-11-06 10:23:18 +0900

    ルーティング配置変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  get 'messages/new'
   devise_for :users, controllers: {
     registrations: 'users/registrations'
   }
@@ -10,6 +9,7 @@ Rails.application.routes.draw do
 
   root to: "places#index"
   resources :users
-
+  
+  get 'messages/new'
   resources :messages, only: [:new, :create]
 end 
