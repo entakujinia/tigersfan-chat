5f4565b32a2184586358f598db99c52a8903d40f
entakujinia <takusnn@icloud.com>
2020-11-07 11:47:23 +0900

    トーク相手のプルダウン実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,8 +8,8 @@ class User < ApplicationRecord
   validates :fan_history,     presence: true
   validates :favorite_player, presence: true
 
-   has_many :room_users
-   has_many :rooms, through: room_users
+  #has_many :room_users
+  # has_many :rooms, through: room_users
   # has_many :messages
 
   extend ActiveHash::Associations::ActiveRecordExtensions
