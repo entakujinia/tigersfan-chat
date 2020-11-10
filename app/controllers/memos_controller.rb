91cdae94fb774d143bea2c261f1e2de8fe5923e0
entakujinia <takusnn@icloud.com>
2020-11-10 21:33:18 +0900

    createアクションとjsファイルの作成

--- app/controllers/memos_controller.rb
+++ app/controllers/memos_controller.rb
@@ -2,4 +2,10 @@ class MemosController < ApplicationController
   def index
      @memo = Memo.all.order(id: "DESC")
   end
+
+  def create
+    Memo.create(content: params[:content])
+    redirect_to action: :index
+  end
+ 
 end
