6755890d96031abb9f36f2d7c2f6d7f8805c4211
entakujinia <takusnn@icloud.com>
2020-11-10 17:37:06 +0900

    typo修正とファーストビューへのパス修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,12 +12,7 @@ class UsersController < ApplicationController
     end
   end
 
-  def destroy
-    user = User.find(params[:id])
-    user.destroy
-  end
-
-  private
+ private
 
   def user_params
     params.require(:user).permit(:nickname, :email, :password, :password_confirmation, :fan_history_id, :favorite_player_id)
