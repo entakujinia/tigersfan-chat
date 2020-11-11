2d705d5ff688ab9093fef5cdaee0998838c82ddb
entakujinia <takusnn@icloud.com>
2020-11-11 11:02:21 +0900

    クリック動作実装

--- app/javascript/packs/checked.js
+++ app/javascript/packs/checked.js
@@ -1,5 +1,8 @@
 function check() {
-  console.log("OK")
   const posts = document.querySelectorAll(".post");
-};
+  posts.forEach(function (post) {
+    post.addEventListener("click", () => { 
+    });
+  });
+}
 window.addEventListener("load", check);
