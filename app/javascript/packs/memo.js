e14949ef617344b8cd5e43a028a04c7bffc82648
entakujinia <takusnn@icloud.com>
2020-11-11 12:06:08 +0900

    リクエスト送信実装

--- app/javascript/packs/memo.js
+++ app/javascript/packs/memo.js
@@ -1,5 +1,11 @@
 function memo() {
   const submit = document.getElementById("submit");
-  submit.addEventListener("click", (e) => {});
+  submit.addEventListener("click", (e) => {
+    const formData = new FormData(document.getElementById("form"));
+    const XHR = new XMLHttpRequest();
+    XHR.open("POST", "/posts", true);
+    XHR.responseType = "json";
+    XHR.send(formData);
+  });
 }
 window.addEventListener("load", memo);
