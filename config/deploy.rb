99280be8d3b7fa544902081100e8a041afdf0412
entakujinia <takusnn@icloud.com>
2020-11-15 21:53:30 +0900

    自動デプロイのための修正

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@ lock '3.14.1'
 
 set :application, 'tigersfan-chat'
 
-set :repo_url,  'entakujinia/tigersfan-chat.git'
+set :repo_url,  'git@github.com:entakujinia/tigersfan-chat.git'
 
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
