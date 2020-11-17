0196d50439d4d97a462853710cb7ea83452c88cb
entakujinia <takusnn@icloud.com>
2020-11-17 19:34:30 +0900

    新規登録できないとき

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -21,5 +21,25 @@ RSpec.describe 'Users', type: :system do
       expect(current_path).to eq root_path
      end
     end
+    context 'ユーザー新規登録ができないとき' do
+     it '誤った情報ではユーザー新規登録ができない' do
+      visit new_user_registration_path
+      fill_in 'user_nickname', with: ''
+      fill_in 'user_email', with: ''
+      fill_in 'user_password', with: ''
+      fill_in 'user_password_confirmation', with: ''
+      expect{
+       find('input[name="commit"]').click
+       }.to change {User.count}.by(0)
+     end
+     it 'ファン歴と好きな選手を設定しなければユーザー新規登録ができない' do
+      visit new_user_registration_path
+      select '--', from: "user[fan_history_id]"
+      select '--', from: "user[favorite_player_id]"
+      expect{
+       find('input[name="commit"]').click
+       }.to change {User.count}.by(0)
+     end
+    end
   end
 end
