a183c4c3fbea9dbbb41e605efe5ce3e664e0c6fa
entakujinia <takusnn@icloud.com>
2020-11-17 11:52:30 +0900

    チャットルーム単体モデル実装

--- spec/models/room_spec.rb
+++ spec/models/room_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Room, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'チャットルーム作成' do
+    before do
+      @room = FactoryBot.build(:room)
+    end
+
+    it 'nameの値が存在すれば登録できること' do
+      expect(@room).to be_valid
+    end
+
+    it 'nameが空では登録できないこと' do
+      @room.name = ''
+      @room.valid?
+      expect(@room.errors.full_messages).to include("Nameを入力してください")
+    end
+  end
 end
