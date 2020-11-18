3c079ae441be45cfd4a57039d6852272a0a5928b
entakujinia <takusnn@icloud.com>
2020-11-18 16:05:40 +0900

    不要なテーブルを削除
    resetにより
    herokuでの不具合のため

--- db/migrate/20201118062657_create_memos.rb
+++
@@ -1,5 +0,0 @@
-class DeleteRocords < ActiveRecord::Migration[5.1]
-  def change
-    drop_table :memos
-  end
-end
--- db/migrate/20201118062904_memos.rb
+++
@@ -1,4 +0,0 @@
-class Memos < ActiveRecord::Migration[6.0]
-  def change
-  end
-end
