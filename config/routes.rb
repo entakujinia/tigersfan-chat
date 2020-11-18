6cbe705bde77970bab281087fb60cfda56847f7b
entakujinia <takusnn@icloud.com>
2020-11-18 17:58:33 +0900

    ログイン機能修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
+  devise_scope :user do
+    post '/users/guest_sign_in', to: 'users/sessions#new_guest'
+  end
+  
   devise_for :users, controllers: {
     registrations: 'users/registrations'
   }
 
-  devise_scope :user do
-    post '/users/guest_sign_in', to: 'users/sessions#new_guest'
-  end
 
   root to: "places#index"
   get 'rooms/index'
